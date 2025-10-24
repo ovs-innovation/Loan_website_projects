8ac8a104f79621f8ca22c769be992c957e67d86c
Rahul Kumar <developerrahul977@gmail.com>
2025-10-24 16:16:40 +0530

    update today color

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'fibank' directory
+app.use('/assets',express.static(path.join(__dirname, '..', 'assets')));
+app.use(express.static(path.join(__dirname, '..')));
+
+app.use(express.json());
+
+// Route for the home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  
+});
+
+
+// app start
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`📂 Serving files from: ${path.join(__dirname, '..','assets')}`);
+});
