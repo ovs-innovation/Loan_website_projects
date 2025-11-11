aa7ea84935ba4aad05ed28012ffa203c5550e709
Rahul Kumar <152783083+fired-rahul-udp-80@users.noreply.github.com>
2025-11-11 22:49:01 +0530

    Refactor server/index.js to simplify middleware
    Removed static file serving and home route handling.

--- server/index.js
+++ server/index.js
@@ -4,10 +4,6 @@ const path = require('path');
 const PORT = process.env.PORT || 8091;
 const cors = require('cors');
 
-// Serve static files from the 'fibank' directory
-app.use('/assets',express.static(path.join(__dirname, '..', 'assets')));
-app.use(express.static(path.join(__dirname, '..')));
-
 // Middleware
 app.use(cors());  
 app.use(express.json());
@@ -16,17 +12,11 @@ app.use(express.json());
 const routes = require('./routes/route');
 app.use('/api', routes);
 
-// // Route for the home page
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'index.html'));
-  
-// });
-
-
 // app start
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   console.log(`📂 Serving files from: ${path.join(__dirname, '..','assets')}`);
 
 });
+
 
